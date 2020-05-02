90f4fab09cbe75dea4600576bfdc4a2b4f6ac91b
Cyanova <lujiabj@yahoo.fr>
2020-05-02 17:59:02 +0200

    Made new global Table containing flags for whether an object should have its state and visuals updated

--- objects.lua
+++ objects.lua
@@ -4,9 +4,10 @@ local objects = {}
 -- This is why items such as mirrors have 2 states: one for horizontal/vertical mirrors and one for diagonal mirrors. pwheel on the other hand can only take one state (but 2 rotations!)
 -- It is worth noting that color can also influence the image used to represent the object (details to be discussed)
 local Object = {t = 0, id = 0, xpos = nil, ypos = nil, state = 0, rotation = 0, colour = 0, canMove = false, canChangeState = false, canChangeColour = false}
-local Types =     {"wall","glass","source","mirror","dmirror","pwheel","receiver" }
-local numStates = {    15,      1,       2,       2,        2,       1,         2 }
 local ID = {} -- contains the ID of the newest object of each type (i.e. the amount of each types unless some have been deleted); int:ID[int:type]
+local Types =      {"wall","glass","source","receiver","mirror","dmirror","pwheel" }
+local numStates =  {    15,      1,       2,         2,       2,        2,       1 }
+UpdateObjectType = { false,  false,   false,     false,    false,   false,   false }
 ObjectReferences = {} -- contains tables of references to each object of each type sorted by type and ID; Object:ObjectReferences[int:type][int:id]
 
 function Object:new(t,xpos,ypos,state,rotation,colour,canMove,canChangeState,canChangeColour)
@@ -16,6 +17,7 @@ function Object:new(t,xpos,ypos,state,rotation,colour,canMove,canChangeState,can
 
   o.t = Types[t] and t or TYPE_WALL
   ID[o.t] = ID[o.t] + 1
+  UpdateObjectType[o.t] = true
   o.id = ID[o.t]
   o.xpos = xpos or 1
   o.ypos = ypos or 1
@@ -34,6 +36,7 @@ end
 function Object:rightClick(shiftClick)
   shiftClick = shiftClick or false
   if not self.canChangeState then return false end
+  UpdateObjectType[self.t] = true
   self.state = shiftClick and self.state-1 or self.state+1
   self.state = self.state%numStates[self.t]
   return true
@@ -45,6 +48,7 @@ function Object:changePosition(xpos,ypos) -- note that this only changes the pos
 end
 
 function Object:delete()
+  UpdateObjectType[self.t] = true
   ObjectReferences[self.t][self.id] = nil
 end
 
