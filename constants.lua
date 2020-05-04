4a69299f039f23d254360658ea09a082815a12f7
Cyanova <lujiabj@yahoo.fr>
2020-05-04 20:14:47 +0200

    Removed the endless stream of bools when loading

--- constants.lua
+++ constants.lua
@@ -7,9 +7,7 @@ NUM_CONNECTED_TEXTURE_TILES = 2
 STATE_CONFIGURATIONS = {[1]=1,[5]=2,[21]=3,[85]=4,[17]=5,[29]=6,[113]=7,[93]=8,[125]=9,[7]=10,[31]=11,[127]=12,[255]=13,[119]=14,[0]=15}
 
 function file_exists(name)
-   local exists = love.filesystem.exists(name)
-   print(exists)
-   if exists~=false then return true
+   if love.filesystem.getInfo(name) then return true
    else return false end
 end
 
